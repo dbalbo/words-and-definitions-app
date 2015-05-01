84e9521edd99ee1c2af305985ec572416b26bcd3
Debbie <debbalbo@hotmail.com>
2015-05-01 11:50:32 -0700

    after trying to confuse myself, finally I got the working first spec and code to pass. It created a Definition class and set up instance variables to hold 3 definitions of a word entered

--- lib/palabras.rb
+++ lib/palabras.rb
@@ -1,11 +1,29 @@
 class Definition
 
 
-define_method(:initialize) do | definition1 |
-	@definition1 = definition1
-	end	
+define_method(:initialize) do |first, second, third|
+@first = first
+@second = second
+@third = third
 end	
 
-define_method(:definition1) do
-	@definition1
+
+define_method(:first) do
+	@first
+	end
+
 end
+
+
+# class Definition
+
+
+
+# 	attr_reader(:definition1, :definition2, :definition3)
+
+# 	define_method(:initialize) do |attributes|
+# 		@definition1 = attributes.fetch(:definition1)
+# 		@definition2 = attributes.fetch(:definition2)
+# 		@definition3 = attributes.fetch(:definition3)
+# 	end	
+# end
--- spec/palabras_spec.rb
+++ spec/palabras_spec.rb
@@ -1,11 +1,40 @@
 require('rspec')
 require('palabras')
 
-describe('#definition1') do
-	it('returns a definition of the word entered')do
-	test_definition = Definition.new ("another name for cat")
-	expect(test_definition.definition1()).to(eq('another name for cat'))
-
+describe('#first') do
+	it('returns the first definition of a word entered')do
+		test_definition = Definition.new("definition1", "defintion2", "definition3")
+		expect(test_definition.first()).to(eq("definition1"))
 	end
 end
 	
+
+
+
+
+
+
+
+
+
+
+# describe(Definition) do
+	
+
+# end
+
+# describe('#word') do   it('returns a definition of a word entered')do
+# test_defintiion = Defnition.new({:defintion1 => 'something spoken', :defintion2 => 'something written', :definition3 => 'something created'})
+# expect(test_defintion.word()).to(eq('something spoken'))
+# 		end
+# 	end
+
+
+
+
+
+
+	# describe ('definition2') do
+	# 	it('returns a new definition of the word entered')
+	# 	test_definition = Definition.new ('')
+	# end
